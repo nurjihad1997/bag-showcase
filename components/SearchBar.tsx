0058200227aabfd3303ead53f252df40963f987c
JihadFirman <nurjihadfirmanullah@gmail.com>
2024-06-15 12:19:45 +0100

    new update
    new update

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { useState } from "react";
+
+import { SearchManufacturer } from "./";
+
 const SearchBar = () => {
-  const handleSearch = () => {}
+  const [manufacturer, setManufacturer] = useState("");
+  const handleSearch = () => {};
   return (
-    <form className='searchbar' onSubmit={handleSearch}>
+    <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
-        
+        <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer} />
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
